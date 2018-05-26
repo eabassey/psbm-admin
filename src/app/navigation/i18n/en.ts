788a5868ff77294f8fcb24903b410078dce87970
Enoch Abassey <eabassey@yahoo.com>
2018-05-26 20:36:32 +0200

    clean up file 6

--- src/app/navigation/i18n/en.ts
+++ src/app/navigation/i18n/en.ts
@@ -4,22 +4,8 @@ export const locale = {
         'NAV': {
             'APPLICATIONS': 'Applications',
             'DASHBOARDS'  : 'Dashboards',
-            'CALENDAR'    : 'Calendar',
             'ECOMMERCE'   : 'E-Commerce',
-            'ACADEMY'     : 'Academy',
-            'MAIL'        : {
-                'TITLE': 'Mail',
-                'BADGE': '25'
-            },
-            'MAIL_NGRX'        : {
-                'TITLE': 'Mail Ngrx',
-                'BADGE': '13'
-            },
-            'CHAT'        : 'Chat',
-            'FILE_MANAGER': 'File Manager',
             'CONTACTS'    : 'Contacts',
-            'TODO'        : 'To-Do',
-            'SCRUMBOARD'  : 'Scrumboard'
         }
     }
 };
