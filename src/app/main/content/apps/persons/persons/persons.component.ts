d1b6696d984dcca99a92950d1eb841137fa53a73
Enoch Abassey <eabassey@yahoo.com>
2018-06-03 18:10:28 +0200

    changing data

--- src/app/main/content/apps/persons/persons/persons.component.ts
+++ src/app/main/content/apps/persons/persons/persons.component.ts
@@ -9,6 +9,7 @@ import { fuseAnimations } from '@fuse/animations';
 import { FuseUtils } from '@fuse/utils';
 
 import { PersonsService } from './persons.service';
+import { Person } from '../person/person.model';
 
 @Component({
   selector: 'fuse-persons',
@@ -17,12 +18,12 @@ import { PersonsService } from './persons.service';
   animations: fuseAnimations
 })
 export class PersonsComponent implements OnInit {
-  displayedColumns = ['select', 'position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  displayedColumns = ['select', 'firstName', 'lastName'];
+  dataSource = new MatTableDataSource<Partial<Person>>(PERSONS);
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  selection = new SelectionModel<PeriodicElement>(true, []);
+  selection = new SelectionModel<Partial<Person>>(true, []);
   constructor(private personsService: PersonsService) {}
 
   ngOnInit() {
@@ -50,72 +51,77 @@ export class PersonsComponent implements OnInit {
       : this.dataSource.data.forEach(row => this.selection.select(row));
   }
 }
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
-  { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
-  { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
-  { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
-  { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
-  { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
-  { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
-  { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
-  { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
-  { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
-  { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
-  { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
-  { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
-  { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
-  { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
-  { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
-  { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
-  { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
-  { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
-  { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
-  { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
-  { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
-  { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
-  { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
-  { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
-  { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
-  { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
-  { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' }
+const PERSONS: Partial<Person>[] = [
+  { firstName: 'Yae', lastName: 'Pert' },
+  { firstName: 'Henry', lastName: 'Oduro' }
 ];
+// export interface PeriodicElement {
+//   name: string;
+//   position: number;
+//   weight: number;
+//   symbol: string;
+// }
+
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+//   { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+//   { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+//   { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+//   { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+//   { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+//   { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+//   { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+//   { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+//   { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+//   { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
+//   { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
+//   { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
+//   { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
+//   { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
+//   { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
+//   { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
+//   { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
+//   { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
+//   { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
+//   { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+//   { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+//   { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+//   { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+//   { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+//   { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+//   { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+//   { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+//   { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+//   { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+//   { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
+//   { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
+//   { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
+//   { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
+//   { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
+//   { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
+//   { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
+//   { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
+//   { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
+//   { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
+//   { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+//   { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+//   { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+//   { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+//   { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+//   { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+//   { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+//   { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+//   { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+//   { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+//   { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
+//   { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
+//   { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
+//   { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
+//   { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
+//   { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
+//   { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
+//   { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
+//   { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
+//   { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' }
+// ];
